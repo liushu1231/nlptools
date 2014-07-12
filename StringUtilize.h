6e332e82107dabc7aab1eedf9dab56a476b7f64d
liushu1231 <liushu.fly@gmail.com>
2014-07-13 00:03:58 +0800

    modify StringUtilize.h for only declear the class

--- StringUtilize.h
+++ StringUtilize.h
@@ -1,54 +1,20 @@
 
 
 #include<string>
-#include<iostream>
 #include<vector>
-#include<stdio.h>
-using namespace std;
 
+#define N 100
 class StringUtilizes {
 public:
-	static void StringTrim(string &s){
-		size_t left = 0;
-		size_t right = s.length()-1;
-		while((s[left] == ' ' || s[left] == '\r' || s[left] == '\n' ) && left < s.length())
-        {
-        	left++;
-        }
-        while((s[right] == ' '|| s[right] == '\r' || s[right] == '\n' ) && right >= 0)
-        {
-        	right--;
-        }
-        if(left > right)
-        {
-        	s = "";
-        	return;
-        }
-	}
-	static void StringSplit(vector<string>& parts, string &str, const char* splitChars)
-	{
-		parts.clear();
-		int split_len = strlen(splitChars);
-		size_t last_pos = 0;
-		size_t pos = str.find(splitChars,last_pos);
+	static int matrix[N][N];
+public:
+	static bool StringTrim(std::string &s);
+	static bool StringSplit(std::vector<std::string>& parts, std::string &str, const char* splitChars);
 
-		while( pos != string::npos)
-		{
-			if(pos != last_pos)
-			{
-				//cout<<str.substr(last_pos,pos-last_pos)<<endl;
-				parts.push_back(str.substr(last_pos,pos-last_pos));
-			}
-			last_pos = pos + split_len;
-			//cout<<last_pos<<endl;
-			pos = str.find(splitChars,last_pos);
-		}
+	static inline int GetMaxofTwo(int m, int n);
+	static inline int GetMinofTwo(int m,int n);
+	static inline int GetMinOfThree(int m, int n, int k);
 
-		//process the last part
-		if(last_pos + split_len < str.length())
-		{
-			parts.push_back(str.substr(last_pos,str.length()-last_pos));
-		}
-	}
-
+	static int GetEditDis(std::string leftStr, std::string rightStr);
+	static std::string GetMaxCommonPrefix(std::string leftStr,std::string rightStr);
 };
